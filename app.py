7f0ec70c505b735ca58388f30ac0274b34b2b4a7
quanvparadium <quan.vparoger@gmail.com>
2023-11-09 14:31:43 +0700

    feat: add Rule-based MT

--- app.py
+++ app.py
@@ -1,19 +1,62 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from starlette.responses import FileResponse 
 from pydantic import BaseModel
-from inference import translate_vi2en
+from starlette.responses import FileResponse 
+from fastapi.staticfiles import StaticFiles
+
+from urbans import Translator
+
+src_to_target_grammar =  {
+    "NP -> JJ NN": "NP -> NN JJ" # in Vietnamese NN goes before JJ
+}
+
+en_to_vi_dict = {
+    "I":"tôi",
+    "love":"yêu",
+    "hate":"ghét",
+    "dogs":"những chú_chó",
+    "good":"ngoan",
+    "bad":"hư"
+    }
+# import torch
+# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+# tokenizer_vi2en = AutoTokenizer.from_pretrained("vinai/vinai-translate-vi2en", src_lang="vi_VN")
+# model_vi2en = AutoModelForSeq2SeqLM.from_pretrained("vinai/vinai-translate-vi2en")
+# device_vi2en = torch.device("cpu")
+# model_vi2en.to(device_vi2en)
+# def translate_vi2en(vi_texts: str) -> str:
+#     input_ids = tokenizer_vi2en(vi_texts, padding=True, return_tensors="pt").to(device_vi2en)
+#     output_ids = model_vi2en.generate(
+#         **input_ids,
+#         decoder_start_token_id=tokenizer_vi2en.lang_code_to_id["en_XX"],
+#         num_return_sequences=1,
+#         num_beams=5,
+#         early_stopping=True
+#     )
+#     en_texts = tokenizer_vi2en.batch_decode(output_ids, skip_special_tokens=True)
+#     return en_texts
+
+
+
+def translate_vi2en(vi_texts: str) -> str:
+    return "STRING: " + vi_texts
+
+def process_grammar(grammar: str) -> str:
+    grammarLst = grammar.strip().split('\n')
+    print(grammarLst)
+    grammarLst_new = list(map(lambda x: x[4:], grammarLst))
+    output = '\n'.join(grammarLst_new)
+    print(output)
+    return output
 
 STATIC_FILES_DIR = './templates/'
 app = FastAPI()
 
 class InputModel(BaseModel):
     sentence: str
+    grammar: str
+    method: int
 
-class OutputModel(BaseModel):
-    language: str
-
-@app.get("/", response_class=FileResponse)
+@app.get('/')
 async def home():
     # return FileResponse('home.html')
     return FileResponse(
@@ -21,22 +64,27 @@ async def home():
         headers={"Cache-Control": "no-cache"}
     )
 
-app.mount("/", StaticFiles(directory=STATIC_FILES_DIR), name="templates")
 
+@app.post('/predict')
+def predict(input: InputModel):
+    print("INPUT SENTENCE: ", input.sentence)
+    print("INPUT METHOD:", input.method)
 
-@app.get('/predict', response_model= OutputModel)
-async def predict(input: InputModel):
-    language = translate_vi2en(input.sentence)
-    print("INPUT: ", input.sentence)
-    print("RESULT: ", language)
+    if input.method == 0:
+        translator = Translator(src_grammar = process_grammar(input.grammar),
+                        src_to_tgt_grammar = src_to_target_grammar,
+                        src_to_tgt_dictionary = en_to_vi_dict)
+        language = translator.translate(input.sentence) 
+    elif input.method == 1:
+        language = "METHOD Statistical" + input.sentence
+    elif input.method == 2:
+        language = translate_vi2en(input.sentence)
     return {
-        "target_text": language
+        "output": language
     }
 
-if __name__ == "__main__":
+app.mount("/", StaticFiles(directory=STATIC_FILES_DIR, html=True))
+
+if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='127.0.0.1', port=8000)
-    
-
-
-
--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -1,42 +1,86 @@
-const inputText = document.getElementById("source_text");
-console.log(inputText)
-// console.log(inputText.value)
-// inputText.addEventListener("input", function() {
-//     const inputVal = inputText.value;
-//     const outputElement = document.getElementById("target-text");
-//     outputElement.textContent = inputVal;
-// })
+var method = 2;
+const temp = {
+    "Rule=based Machine Translation": '',
+    "Statistical Machine Translation": '',
+    "Neural Machine Translation": ''
+}
 
-var activeBtn = document.querySelectorAll('button.model');
-console.log(activeBtn)
-for (var i = 0; i < activeBtn.length; ++i)
-activeBtn[i].addEventListener('click', function() {
-    console.log(activeBtn[i]);
-    activeBtn[i].classList.add('active');
+const inputText = document.getElementById("source_text");
+var modelBtn = document.querySelectorAll('.model');
+for (let i = 0; i < modelBtn.length; ++i){
+    modelBtn[i].addEventListener('click', function() {
+        
+        const grammarArea = document.getElementById('source_grammar')
+        const checkActive = document.querySelector('.active');
+
+        // REMOVE ACTIVE OLD MODEL
+        checkActive.classList.remove('active')
+        if (checkActive.innerText == "Rule-based Machine Translation") {
+            grammarArea.style.display = "none";
+            document.querySelector('textarea').style.minHeight = "100px";
+        }
+        temp[checkActive.innerText] = document.querySelector('textarea').value
+
+
+        // CHOOSE ACTIVE NEW MODEL
+        modelBtn[i].classList.add('active')
+        if (modelBtn[i].innerText == "Rule-based Machine Translation"){
+            method = 0;
+            grammarArea.style.display = "inline-block";
+            document.querySelector('textarea').style.minHeight = "200px";
+        }
+        else if (modelBtn[i].innerText == "Statistical Machine Translation") {
+            method = 1;
+        }
+        else method = 2;
+
+        if (temp[modelBtn[i].innerText] === undefined) {
+            // console.log("NOT ALLOW");
+            document.querySelector('textarea').value = ''
+        }
+        else document.querySelector('textarea').value = temp[modelBtn[i].innerText]
+    })
+}
+
+
+const languageOption = document.getElementById('target_language');
+languageOption.addEventListener('change', function() {
+    console.log(languageOption.value);
 })
 
+const grammarInput = document.getElementById('source_grammar')
 
 
+
+// TRANSLATE BUTTON
 const submitTrans = document.getElementById('submitTrans');
-console.log(submitTrans)
 submitTrans.addEventListener('click', function() {
-    fetch('http://127.0.0.1:8000/predict', {
+    console.log("OKK Click")
+    console.log(`
+        SENTENCE: ${inputText.value},
+        GRAMMAR: ${grammarInput.value},
+        METHOD: ${method}
+    `)
+    fetch('/predict', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            text: inputText.value
+            sentence: inputText.value,
+            grammar: grammarInput.value,
+            method: method,
         }),
     })
     .then(response => response.json())
     .then(data => {
-        const outputElement = document.getElementById("target-text");
-        outputElement.textContent = data.target_text       
+        const outputElement = document.getElementById("target_text");
+        console.log(outputElement)
+        outputElement.innerText = data.output       
     })
     .catch((error) => {
-        const outputElement = document.getElementById("target-text");        
-        outputElement.textContent = "Error"      
+        const outputElement = document.getElementById("target_text");        
+        outputElement.innerText = "Error"      
         
     })
 })
