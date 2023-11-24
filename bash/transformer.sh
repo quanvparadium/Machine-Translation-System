9a2412685b38781fb04b908cf78803f4496a2ab3
quanvparadium <quan.vparoger@gmail.com>
2023-11-24 23:43:25 +0700

    add colab file

--- bash/transformer.sh
+++ bash/transformer.sh
@@ -1,6 +1,6 @@
 python train.py \
-    --model_name 'Transformer' --model_type 'unigram' --device 'mps'\
-    --num_heads 8 --num_layers 6 --d_model 512 --d_ff 2048 --drop_out 0.1 \\
+    --model_name 'Transformer' --model_type 'unigram'\
+    --num_heads 8 --num_layers 6 --d_model 512 --d_ff 2048 --drop_out 0.1 \
     --batch_size 16 --epochs 1 \
     --src_lang 'vi' --tgt_lang 'en' --seq_len 150 \
     --ckpt_path 'pretrained' --ckpt_name 'transformer_ep1.pt'\
